25b726b42b9e264cddc2dd1614b257e369917ec9
Erik S. Carlsten <ecarlste@gmail.com>
2015-04-29 13:05:26 -0500

    Created project and added functionality to read in test cases

---
+++ UtopianTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UtopianTree
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int testCount = Int32.Parse(Console.ReadLine());
+            int[] testCases = new int[testCount];
+
+            for (int i = 0; i < testCount; i++)
+            {
+                testCases[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            foreach (int testCase in testCases)
+            {
+
+            }
+        }
+    }
+}
