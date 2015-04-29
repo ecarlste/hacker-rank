05985840ff3260346e731827b518047887beb9b3
Erik S. Carlsten <ecarlste@gmail.com>
2015-04-29 15:24:27 -0500

    Created ServiceLine project and added input reading

---
+++ ServiceLane/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLane
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            String[] input = Console.ReadLine().Split(' ');
+            int testCaseCount = Int32.Parse(input[1]);
+
+            input = Console.ReadLine().Split(' ');
+            int[] serviceLaneWidths = Array.ConvertAll(input, s => Int32.Parse(s));
+        }
+    }
+}
