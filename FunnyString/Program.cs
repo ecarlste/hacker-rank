21a9f1143d8568224f2de6f4fa53efd7db15d503
Erik S. Carlsten <ecarlste@gmail.com>
2015-04-29 12:47:26 -0500

    Built IsFunnyString method that solves basic test case

--- FunnyString/Program.cs
+++ FunnyString/Program.cs
@@ -8,8 +8,53 @@ namespace FunnyString
 {
     class Program
     {
+        static bool IsFunnyString(string testString)
+        {
+            bool isFunny = true;
+
+            int leftIndex = 1;
+            int rightIndex = testString.Length - 2;
+
+            while (isFunny && leftIndex <= rightIndex)
+            {
+                int leftChange = testString[leftIndex] - testString[leftIndex - 1];
+                int rightChange = testString[rightIndex] - testString[rightIndex + 1];
+
+                if (Math.Abs(leftChange) != Math.Abs(rightChange))
+                {
+                    isFunny = false;
+                }
+                else
+                {
+                    leftIndex++;
+                    rightIndex--;
+                }
+            }
+
+            return isFunny;
+        }
+
         static void Main(string[] args)
         {
+            int numberOfTestStrings = Int32.Parse(Console.ReadLine());
+            string[] testStrings = new string[numberOfTestStrings];
+
+            for (int i = 0; i < numberOfTestStrings; i++)
+            {
+                testStrings[i] = Console.ReadLine();
+            }
+
+            foreach (string testString in testStrings)
+            {
+                if (IsFunnyString(testString))
+                {
+                    Console.WriteLine("Funny");
+                }
+                else
+                {
+                    Console.WriteLine("Not Funny");
+                }
+            }
         }
     }
 }
