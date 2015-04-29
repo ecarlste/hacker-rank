e800085b3c2f65819da4fc373ad6f0a715249c59
Erik S. Carlsten <ecarlste@gmail.com>
2015-04-29 16:00:13 -0500

    Added FindMinWidthInRange method and LaneRange class

--- ServiceLane/Program.cs
+++ ServiceLane/Program.cs
@@ -6,15 +6,61 @@ using System.Threading.Tasks;
 
 namespace ServiceLane
 {
+    class LaneRange
+    {
+        int enterIndex;
+        public int EnterIndex
+        {
+            get { return enterIndex; }
+            set { enterIndex = value; }
+        }
+
+        int exitIndex;
+        public int ExitIndex
+        {
+            get { return exitIndex; }
+            set { exitIndex = value; }
+        }
+
+        public LaneRange(int enterIndex, int exitIndex)
+        {
+            this.enterIndex = enterIndex;
+            this.exitIndex = exitIndex;
+        }
+    }
+
     class Solution
     {
         static void Main(string[] args)
         {
             String[] input = Console.ReadLine().Split(' ');
-            int testCaseCount = Int32.Parse(input[1]);
+            int laneRangeCount = Int32.Parse(input[1]);
 
             input = Console.ReadLine().Split(' ');
             int[] serviceLaneWidths = Array.ConvertAll(input, s => Int32.Parse(s));
+
+            LaneRange[] laneRanges = new LaneRange[laneRangeCount];
+            for (int i = 0; i < laneRanges.Length; i++)
+            {
+                input = Console.ReadLine().Split(' ');
+                laneRanges[i] = new LaneRange(Int32.Parse(input[0]), Int32.Parse(input[1]));
+            }
+
+            foreach (LaneRange laneRange in laneRanges)
+            {
+                int maxWidth = FindMinWidthInRange(laneRange, serviceLaneWidths);
+                Console.WriteLine(maxWidth);
+            }
+        }
+
+        private static int FindMinWidthInRange(LaneRange laneRange, int[] laneWidths)
+        {
+            int laneWidthsInRangeCount = laneRange.ExitIndex - laneRange.EnterIndex + 1;
+            int[] laneWidthsInRange = new int[laneWidthsInRangeCount];
+            
+            Array.Copy(laneWidths, laneRange.EnterIndex, laneWidthsInRange, 0, laneWidthsInRangeCount);
+
+            return laneWidthsInRange.Min();
         }
     }
 }
