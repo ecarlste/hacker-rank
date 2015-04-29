3befdc8b38190d8cc56edafb247159290cf37008
Erik S. Carlsten <ecarlste@gmail.com>
2015-04-29 13:19:04 -0500

    Created method to calculate tree height after N seasons

--- UtopianTree/Program.cs
+++ UtopianTree/Program.cs
@@ -6,6 +6,12 @@ namespace UtopianTree
 {
     class Solution
     {
+        enum Seasons
+        {
+            Summer,
+            Spring
+        };
+
         static void Main(string[] args)
         {
             int testCount = Int32.Parse(Console.ReadLine());
@@ -18,8 +24,30 @@ namespace UtopianTree
 
             foreach (int testCase in testCases)
             {
-
+                Console.WriteLine(FindTreeHeightAfterSeasons(testCase));
             }
+        }
+
+        private static int FindTreeHeightAfterSeasons(int numberOfSeasons)
+        {
+            Seasons currentSeason = Seasons.Spring;
+            int treeHeight = 1;
+
+            for (int seasonsLeft = numberOfSeasons; seasonsLeft > 0; seasonsLeft--)
+            {
+                if (currentSeason == Seasons.Spring)
+                {
+                    treeHeight *= 2;
+                    currentSeason = Seasons.Summer;
+                }
+                else
+                {
+                    treeHeight += 1;
+                    currentSeason = Seasons.Spring;
+                }
+            }
+
+            return treeHeight;
         }
     }
 }
