9cf032cc04bc289e535b0e2b73e1654617b15897
Erik S. Carlsten <ecarlste@gmail.com>
2015-04-30 17:02:13 -0500

    Added console reading and method stub for changing to palindrome

--- TheLoveLetterMystery/Program.cs
+++ TheLoveLetterMystery/Program.cs
@@ -10,6 +10,24 @@ namespace TheLoveLetterMystery
     {
         static void Main(string[] args)
         {
+            int testCaseCount = Int32.Parse(Console.ReadLine());
+            string[] testStrings = new string[testCaseCount];
+
+            for (int i = 0; i < testCaseCount; i++)
+            {
+                testStrings[i] = Console.ReadLine();
+            }
+
+            foreach (string testString in testStrings)
+            {
+                int minimumChanges = DetermineMinimumChangesForPalidrome(testString);
+                Console.WriteLine(testString);
+            }
+        }
+
+        private static int DetermineMinimumChangesForPalidrome(string testString)
+        {
+            return 0;
         }
     }
 }
