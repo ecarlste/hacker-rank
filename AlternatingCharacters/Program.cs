f0c38ed3e542f5b19a53aacc02109efc79a780f6
Erik S. Carlsten <ecarlste@gmail.com>
2015-05-01 11:57:15 -0500

    Added method FindDeletionsneededForAlternating, finished IO

--- AlternatingCharacters/Program.cs
+++ AlternatingCharacters/Program.cs
@@ -10,6 +10,24 @@ namespace AlternatingCharacters
     {
         static void Main(string[] args)
         {
+            int testCaseCount = Int32.Parse(Console.ReadLine());
+            string[] testStrings = new string[testCaseCount];
+
+            for (int i = 0; i < testCaseCount; i++)
+            {
+                testStrings[i] = Console.ReadLine();
+            }
+
+            foreach (string testString in testStrings)
+            {
+                int numberOfDeletions = FindDeletionsNeededForAlternating(testString);
+                Console.WriteLine(numberOfDeletions);
+            }
+        }
+
+        private static int FindDeletionsNeededForAlternating(string testString)
+        {
+            throw new NotImplementedException();
         }
     }
 }
